7f9f7362af5635697da21ad71c42b966561cc11f
sampadrout <sampad.rout@gmail.com>
2022-06-23 23:10:22 +0530

    fines updated

--- src/test/java/io/testproject/generated/tests/serviceprouitest/C57505adminoperatorClaimsDashboardPage.java
+++ src/test/java/io/testproject/generated/tests/serviceprouitest/C57505adminoperatorClaimsDashboardPage.java
@@ -36,7 +36,7 @@ public class C57505adminoperatorClaimsDashboardPage implements ExceptionsReporte
   static void setup() throws Exception {
     URL remoteAddress = new URL("http://0.0.0.0:8686");
 //    driver = new RemoteWebDriver(remoteAddress, "71tH-tyQRbWL-gZCfrhZWBEPha_v-AGyu1aJGwBnQEY1", new ChromeOptions(), "Sampad Rout", ReportType.LOCAL);
-    driver = new ChromeDriver(new ChromeOptions(), ReportType.LOCAL);
+    driver = new ChromeDriver("71tH-tyQRbWL-gZCfrhZWBEPha_v-AGyu1aJGwBnQEY1", new ChromeOptions(), ReportType.LOCAL);
   }
 
   /**
